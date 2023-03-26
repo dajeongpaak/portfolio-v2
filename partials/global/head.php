c74cc52ddb74f712506b2084b0239973b8e04725
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-03-25 22:32:59 -0700

    metadata

--- partials/global/head.php
+++ partials/global/head.php
@@ -4,14 +4,26 @@
 ?>
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title_tag;?></title>
+    <meta name="robots" content="all" />
+    <meta name="description" content="Front-end developer who designs and develops immersive web experience that fully engages users.">
+    <meta name="author" content="Dajeong Park">
+    <meta name="keywords" content="Developer, Designer,front-end, Vancouver">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   
+
+    <meta property="og:title" content="Dajeong Park |front-end developer portfolio" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://dajeongpark.com" />
+    <meta property="og:image" content="<?php echo get_url("/images/main-logo.png"); ?>"/>
+
     <link rel="stylesheet" href="<?php echo get_url('styles/reset.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_url('styles/main.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_url('styles/partials/header.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_url('styles/partials/footer.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_url('styles/partials/' . $partial_css . '.css'); ?>">
     <link rel="icon" sizes="16x16" type="image/svg+xml" href="<?php echo get_url("/images/favicon.svg");?>">
-    <title><?php echo $title_tag;?></title>
+    
     <script src="https://kit.fontawesome.com/0a2ba5e508.js" crossorigin="anonymous"></script>
 </head>
