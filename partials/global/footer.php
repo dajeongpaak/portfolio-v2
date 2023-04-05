5d9e76f8fef5d80cea2711e20cc7b270ef12a81c
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-04-04 21:43:39 -0700

    footer

--- partials/global/footer.php
+++ partials/global/footer.php
@@ -1,28 +1,27 @@
 <footer class="footer border-radius">
     <div class="container">
-    <div class="grid">
-        <div class="footer__content col-12">
+        <div class="footer__content flex">
             <ul>
+                <li> 
+                    <a href="mailto:hello@dajeongpark.com" title="dajeong park email contact me!" class="h6">hello@dajeongpark.com</a>
+                </li>
                 <li>
-                    <span class="h4">
+                    <span class="h6">
                         <a href="https://www.linkedin.com/in/dajeong-park-4956bb255/" target="_blank" title="Dajeong Park Linkedin">Linkedin</a>
                     </span>
                 </li>
                 <li>
-                    <span class="h4">
+                    <span class="h6">
                         <a href="https://github.com/dajeongpaak" target="_blank" title="Dajeong Park GitHub">GitHub</a>
                     </span>
                 </li>
-                <li>
-                    <span class="h4">
-                        <a href="mailto:hello@dajeongpark.com">hello@dajeongpark.com</a>
-                    </span>
-                </li>
             </ul>
-
+            <div> 
+                <a href="#" class="h6">
+                        Back To Top
+                </a>
+            </div>
         </div>
-    </div>
-
     </div>
     <span class="footer__copy h6">© <?php echo date("Y")?> Dajeong Park </span>
 </footer>
